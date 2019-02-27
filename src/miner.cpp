b3c8f6e0a66e44d50df40d821b12059a2da6a30e
DMT <dimapuz97@gmail.com>
2019-02-27 12:47:44 +0200

    1.0.0

--- src/miner.cpp
+++ src/miner.cpp
@@ -118,15 +118,15 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
 
     // Make sure to create the correct block version after zerocoin is enabled
     bool fZerocoinActive = nHeight >= Params().Zerocoin_StartHeight();
-    pblock->nVersion = 5;   // Supports CLTV activation
+    //pblock->nVersion = 5;   // Supports CLTV activation
 
     // -regtest only: allow overriding block.nVersion with
     // -blockversion=N to test forking scenarios
     if (Params().MineBlocksOnDemand()) {
         if (fZerocoinActive)
-            pblock->nVersion = 5;
+            pblock->nVersion = 4;
         else
-            pblock->nVersion = 3;
+            pblock->nVersion = 1;
 
         pblock->nVersion = GetArg("-blockversion", pblock->nVersion);
     }
